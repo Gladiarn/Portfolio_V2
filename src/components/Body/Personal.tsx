230fdee52054c033892a121ad97c781315d5fe46
Ianne Carl <bulilaniannecarl@gmail.com>
2026-03-14 19:40:04 +0800

    add extra info navbar, install shadcn

--- src/components/Body/Personal.tsx
+++ src/components/Body/Personal.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
 
 const Personal = () => {
   return (
-    <div className='canvas-container mx-auto'>
+    <div className='canvas-container mx-auto bg-card h-screen p-4 sm:p-6 md:p-8 lg:p-12 border-l border-r border-b border-border-subtle'>
       personal
     </div>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,6 +8,14 @@ import { useTheme } from "next-themes";
 import Slider from "./Slider";
 import { useModeStore } from "@/hooks/useModeStore";
 import { TbHexagonLetterG } from "react-icons/tb";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 const SOCIAL_LINKS = [
   {
     id: 1,
@@ -32,6 +40,13 @@ const SOCIAL_LINKS = [
 const NAVIGATIONS = ["About", "Projects", "Experience"];
 
 const Navbar = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(timer);
+  }, []);
+
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
@@ -70,35 +85,57 @@ const Navbar = () => {
       {/* socials main container*/}
       <div className="bg-card border-b border-border-subtle flex h-9.25">
         {/* socials inner container */}
-        <div className="flex canvas-container items-center justify-end gap-4">
-          {/* Theme Toggle */}
+        <div className="flex canvas-container items-center justify-between">
+          {/* Footer for header taglines small */}
+          <div className="flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-3">
+            <span className="text-secondary/60 text-[9px] uppercase tracking-widest font-semibold">
+              Leyte, PH —{" "}
+              {time.toLocaleTimeString("en-US", {
+                hour: "2-digit",
+                minute: "2-digit",
+                hour12: true,
+              })}
+            </span>
 
-          <Slider
-            isActive={!isDark}
-            onToggle={() => setTheme(isDark ? "light" : "dark")}
-            leftLabel="Dark"
-            rightLabel="Light"
-            labelClass="text-[10px]"
-            containerClass="px-3 py-1.25 gap-4 bg-background"
-            pillWidth="w-[46%]"
-            activeTranslate="98%"
-          />
-          <div className="w-px h-4.5 bg-border-subtle"></div>
-          <div className="flex gap-3 items-center">
-            {/* socials links */}
-            {SOCIAL_LINKS.map((link) => (
-              <a
-                href={link.href}
-                key={link.id}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <link.icon
-                  size={16}
-                  className="text-secondary/75 hover:text-secondary"
-                />
-              </a>
-            ))}
+            <div className="hidden sm:block w-px h-2.5 bg-border-subtle/50" />
+
+            <span className="text-foreground/50 text-[9px] uppercase tracking-widest font-semibold flex items-center gap-2">
+              <span className="relative flex h-1 w-1">
+                <span className="animate-[ping_3s_linear_infinite] absolute inline-flex h-full w-full rounded-full bg-green-500/30"></span>
+                <span className="relative inline-flex rounded-full h-1 w-1 bg-green-500/60"></span>
+              </span>
+              Open for projects
+            </span>
+          </div>
+          <div className="flex gap-4">
+            {/* Theme Toggle */}
+            <Slider
+              isActive={!isDark}
+              onToggle={() => setTheme(isDark ? "light" : "dark")}
+              leftLabel="Dark"
+              rightLabel="Light"
+              labelClass="text-[10px]"
+              containerClass="px-3 py-1.25 gap-4 bg-background"
+              pillWidth="w-[46%]"
+              activeTranslate="98%"
+            />
+            <div className="w-px h-4.5 bg-border-subtle"></div>
+            <div className="flex gap-3 items-center">
+              {/* socials links */}
+              {SOCIAL_LINKS.map((link) => (
+                <a
+                  href={link.href}
+                  key={link.id}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <link.icon
+                    size={16}
+                    className="text-secondary/75 hover:text-secondary"
+                  />
+                </a>
+              ))}
+            </div>
           </div>
         </div>
       </div>
@@ -107,9 +144,9 @@ const Navbar = () => {
       <div className="bg-background border-b border-border-subtle h-16.5 flex items-center">
         {/* main nav inner container */}
         <div className="flex canvas-container items-center justify-between">
-          <div className="flex justify-between w-full pr-6">
+          <div className="flex justify-between w-full px-6">
             {/* navigation */}
-            <div className="flex gap-4 items-center">
+            <div className="flex gap-15 items-center">
               <div className="flex gap-2 items-center group">
                 {/* Icon */}
                 <TbHexagonLetterG
@@ -122,8 +159,6 @@ const Navbar = () => {
                   Gladiarn
                 </p>
               </div>
-
-              <div className="w-px h-4.5 bg-border-subtle"></div>
 
               <div className="flex gap-8">
                 {NAVIGATIONS.map((nav) => (
